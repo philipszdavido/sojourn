58f605ad71b0a68c84a58350e8d5e8bb642fce52
Chidume Nnamdi <kurtwanger40@gmail.com>
2024-08-04 17:47:01 +0100

    Added tests

---
+++ test/test-main.ts
@@ -0,0 +1,28 @@
+import {Tokenizer} from "../src/tokenizer/tokenizer";
+import {sojourn} from "../src/sojourn/sojourn";
+
+const html = `
+
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- This is a comment -->
+  <title>Document Title</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="styles.css">
+  <script src="script.js" defer></script>
+</head>
+<body>
+  <h1>Hello, World!</h1>
+</body>
+</html>
+
+`
+
+const tokens = new Tokenizer(html).start();
+
+console.log(tokens);
+
+const nodes = sojourn(tokens)
+
+console.log(JSON.stringify(nodes));
